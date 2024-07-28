36ce5ecabf2ba65117f0772507d0de92d1b5e090
IronEva2312 <agusmaty23@gmail.com>
2024-07-27 22:10:03 -0500

    Estilo de pantalla de Creacion de Funciones

--- src/pantallasPrincipales/AdminPantallas/CrearFunciones.java
+++ src/pantallasPrincipales/AdminPantallas/CrearFunciones.java
@@ -1,2 +1,25 @@
-package pantallasPrincipales.AdminPantallas;public class CrearFunciones {
+package pantallasPrincipales.AdminPantallas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CrearFunciones {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton guardarButton;
+    private JButton button1;
+
+    public CrearFunciones() {
+        guardarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
