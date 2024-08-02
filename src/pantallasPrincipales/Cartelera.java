bd538e90b1c1488a36f87804343f9d853a1359da
IronEva2312 <agusmaty23@gmail.com>
2024-08-02 18:18:25 -0500

    Botoncito Log Out Clientes

--- src/pantallasPrincipales/Cartelera.java
+++ src/pantallasPrincipales/Cartelera.java
@@ -17,6 +17,7 @@ public class Cartelera extends JFrame {
     private JPanel CarteleraPanel;
     private JPanel PanelInformacion; // Solo modificaremos este panel
     private JScrollPane PanelScroll;
+    private JButton Salir;
 
     /**
      * Constructor de la clase {@code Cartelera}.
